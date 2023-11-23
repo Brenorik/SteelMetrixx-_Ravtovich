b35e415ab464eff518b2442c3ae0f3403794e16c
Brenorik <deman199191@mail.ru>
2023-11-23 22:15:00 +0300

    23/11

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.burger').onclick = function () {
+// document.querySelector('.menu').classList.add('menu-show');
+document.querySelector('.menu').classList.toggle('menu-show');
+}
