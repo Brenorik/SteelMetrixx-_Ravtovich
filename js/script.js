82888423eb0588cdb040fb69868f786822cc3ed4
Brenorik <deman199191@mail.ru>
2023-11-27 22:09:54 +0300

    27/11v4

--- js/script.js
+++ js/script.js
@@ -68,30 +68,262 @@ document.querySelector('#industry3').onclick = function(){
     
 };
 
+document.querySelector('#industry4').onclick = function(){
+    document.querySelector('#cart1').classList.remove('close');
+    document.querySelector('#cart2').classList.remove('close');
+    document.querySelector('#cart3').classList.remove('close');
+    document.querySelector('#cart4').classList.remove('close');
+    document.querySelector('#cart5').classList.remove('close');
+    document.querySelector('#cart6').classList.remove('close');
+    document.querySelector('#cart7').classList.remove('close');
+    document.querySelector('#cart8').classList.remove('close');
+    document.querySelector('#cart9').classList.remove('close');
+    document.querySelector('#cart10').classList.remove('close');
+    document.querySelector('#cart11').classList.remove('close');
+    document.querySelector('#cart12').classList.remove('close');
+    document.querySelector('#cart1').classList.toggle('close');
+    document.querySelector('#cart2').classList.toggle('close');
+    document.querySelector('#cart4').classList.toggle('close');
+    document.querySelector('#cart5').classList.toggle('close');
+    document.querySelector('#cart6').classList.toggle('close');
+    document.querySelector('#cart7').classList.toggle('close');
+    document.querySelector('#cart8').classList.toggle('close');
+    document.querySelector('#cart9').classList.toggle('close');
+    document.querySelector('#cart11').classList.toggle('close');
+}
 
+document.querySelector('#industry5').onclick = function(){
+    document.querySelector('#cart1').classList.remove('close');
+    document.querySelector('#cart2').classList.remove('close');
+    document.querySelector('#cart3').classList.remove('close');
+    document.querySelector('#cart4').classList.remove('close');
+    document.querySelector('#cart5').classList.remove('close');
+    document.querySelector('#cart6').classList.remove('close');
+    document.querySelector('#cart7').classList.remove('close');
+    document.querySelector('#cart8').classList.remove('close');
+    document.querySelector('#cart9').classList.remove('close');
+    document.querySelector('#cart10').classList.remove('close');
+    document.querySelector('#cart11').classList.remove('close');
+    document.querySelector('#cart12').classList.remove('close');
+    document.querySelector('#cart3').classList.toggle('close');
+    document.querySelector('#cart4').classList.toggle('close');
+    document.querySelector('#cart5').classList.toggle('close');
+    document.querySelector('#cart6').classList.toggle('close');
+    document.querySelector('#cart7').classList.toggle('close');
+    document.querySelector('#cart8').classList.toggle('close');
+    document.querySelector('#cart10').classList.toggle('close');
+    document.querySelector('#cart11').classList.toggle('close');
+    document.querySelector('#cart12').classList.toggle('close');
+}
 
-    // document.querySelector('#cart1').classList.toggle('close');
-    // document.querySelector('#cart2').classList.toggle('close');
-    // document.querySelector('#cart3').classList.toggle('close');
-    // document.querySelector('#cart4').classList.toggle('close');
-    // document.querySelector('#cart5').classList.toggle('close');
-    // document.querySelector('#cart6').classList.toggle('close');
-    // document.querySelector('#cart7').classList.toggle('close');
-    // document.querySelector('#cart8').classList.toggle('close');
-    // document.querySelector('#cart9').classList.toggle('close');
-    // document.querySelector('#cart10').classList.toggle('close');
-    // document.querySelector('#cart11').classList.toggle('close');
-    // document.querySelector('#cart12').classList.toggle('close');
+document.querySelector('#industry6').onclick = function(){
+    document.querySelector('#cart1').classList.remove('close');
+    document.querySelector('#cart2').classList.remove('close');
+    document.querySelector('#cart3').classList.remove('close');
+    document.querySelector('#cart4').classList.remove('close');
+    document.querySelector('#cart5').classList.remove('close');
+    document.querySelector('#cart6').classList.remove('close');
+    document.querySelector('#cart7').classList.remove('close');
+    document.querySelector('#cart8').classList.remove('close');
+    document.querySelector('#cart9').classList.remove('close');
+    document.querySelector('#cart10').classList.remove('close');
+    document.querySelector('#cart11').classList.remove('close');
+    document.querySelector('#cart12').classList.remove('close');
+    document.querySelector('#cart1').classList.toggle('close');
+    document.querySelector('#cart2').classList.toggle('close');
+    document.querySelector('#cart3').classList.toggle('close');
+    document.querySelector('#cart4').classList.toggle('close');
+    document.querySelector('#cart5').classList.toggle('close');
+    document.querySelector('#cart7').classList.toggle('close');
+    document.querySelector('#cart8').classList.toggle('close');
+    document.querySelector('#cart9').classList.toggle('close');
+    document.querySelector('#cart10').classList.toggle('close');
+    document.querySelector('#cart11').classList.toggle('close');
+    document.querySelector('#cart12').classList.toggle('close');
+}
 
-    // document.querySelector('#cart1').classList.remove('close');
-    // document.querySelector('#cart2').classList.remove('close');
-    // document.querySelector('#cart3').classList.remove('close');
-    // document.querySelector('#cart4').classList.remove('close');
-    // document.querySelector('#cart5').classList.remove('close');
-    // document.querySelector('#cart6').classList.remove('close');
-    // document.querySelector('#cart7').classList.remove('close');
-    // document.querySelector('#cart8').classList.remove('close');
-    // document.querySelector('#cart9').classList.remove('close');
-    // document.querySelector('#cart10').classList.remove('close');
-    // document.querySelector('#cart11').classList.remove('close');
-    // document.querySelector('#cart12').classList.remove('close');
+document.querySelector('#industry7').onclick = function(){
+    document.querySelector('#cart1').classList.remove('close');
+    document.querySelector('#cart2').classList.remove('close');
+    document.querySelector('#cart3').classList.remove('close');
+    document.querySelector('#cart4').classList.remove('close');
+    document.querySelector('#cart5').classList.remove('close');
+    document.querySelector('#cart6').classList.remove('close');
+    document.querySelector('#cart7').classList.remove('close');
+    document.querySelector('#cart8').classList.remove('close');
+    document.querySelector('#cart9').classList.remove('close');
+    document.querySelector('#cart10').classList.remove('close');
+    document.querySelector('#cart11').classList.remove('close');
+    document.querySelector('#cart12').classList.remove('close');
+    document.querySelector('#cart1').classList.toggle('close');
+    document.querySelector('#cart2').classList.toggle('close');
+    document.querySelector('#cart3').classList.toggle('close');
+    document.querySelector('#cart4').classList.toggle('close');
+    document.querySelector('#cart6').classList.toggle('close');
+    document.querySelector('#cart7').classList.toggle('close');
+    document.querySelector('#cart8').classList.toggle('close');
+    document.querySelector('#cart9').classList.toggle('close');
+    document.querySelector('#cart10').classList.toggle('close');
+    document.querySelector('#cart11').classList.toggle('close');
+    document.querySelector('#cart12').classList.toggle('close');
+}
+
+document.querySelector('#industry8').onclick = function(){
+    document.querySelector('#cart1').classList.remove('close');
+    document.querySelector('#cart2').classList.remove('close');
+    document.querySelector('#cart3').classList.remove('close');
+    document.querySelector('#cart4').classList.remove('close');
+    document.querySelector('#cart5').classList.remove('close');
+    document.querySelector('#cart6').classList.remove('close');
+    document.querySelector('#cart7').classList.remove('close');
+    document.querySelector('#cart8').classList.remove('close');
+    document.querySelector('#cart9').classList.remove('close');
+    document.querySelector('#cart10').classList.remove('close');
+    document.querySelector('#cart11').classList.remove('close');
+    document.querySelector('#cart12').classList.remove('close');
+    document.querySelector('#cart1').classList.toggle('close');
+    document.querySelector('#cart2').classList.toggle('close');
+    document.querySelector('#cart3').classList.toggle('close');
+    document.querySelector('#cart4').classList.toggle('close');
+    document.querySelector('#cart5').classList.toggle('close');
+    document.querySelector('#cart6').classList.toggle('close');
+    document.querySelector('#cart8').classList.toggle('close');
+    document.querySelector('#cart9').classList.toggle('close');
+    document.querySelector('#cart10').classList.toggle('close');
+    document.querySelector('#cart11').classList.toggle('close');
+    document.querySelector('#cart12').classList.toggle('close');
+}
+
+document.querySelector('#industry9').onclick = function(){
+    document.querySelector('#cart1').classList.remove('close');
+    document.querySelector('#cart2').classList.remove('close');
+    document.querySelector('#cart3').classList.remove('close');
+    document.querySelector('#cart4').classList.remove('close');
+    document.querySelector('#cart5').classList.remove('close');
+    document.querySelector('#cart6').classList.remove('close');
+    document.querySelector('#cart7').classList.remove('close');
+    document.querySelector('#cart8').classList.remove('close');
+    document.querySelector('#cart9').classList.remove('close');
+    document.querySelector('#cart10').classList.remove('close');
+    document.querySelector('#cart11').classList.remove('close');
+    document.querySelector('#cart12').classList.remove('close');
+    document.querySelector('#cart1').classList.toggle('close');
+    document.querySelector('#cart2').classList.toggle('close');
+    document.querySelector('#cart3').classList.toggle('close');
+    document.querySelector('#cart4').classList.toggle('close');
+    document.querySelector('#cart5').classList.toggle('close');
+    document.querySelector('#cart6').classList.toggle('close');
+    document.querySelector('#cart7').classList.toggle('close');
+    document.querySelector('#cart8').classList.toggle('close');
+    document.querySelector('#cart9').classList.toggle('close');
+    document.querySelector('#cart10').classList.toggle('close');
+    document.querySelector('#cart12').classList.toggle('close');
+}
+
+document.querySelector('#industry10').onclick = function(){
+    document.querySelector('#cart1').classList.remove('close');
+    document.querySelector('#cart2').classList.remove('close');
+    document.querySelector('#cart3').classList.remove('close');
+    document.querySelector('#cart4').classList.remove('close');
+    document.querySelector('#cart5').classList.remove('close');
+    document.querySelector('#cart6').classList.remove('close');
+    document.querySelector('#cart7').classList.remove('close');
+    document.querySelector('#cart8').classList.remove('close');
+    document.querySelector('#cart9').classList.remove('close');
+    document.querySelector('#cart10').classList.remove('close');
+    document.querySelector('#cart11').classList.remove('close');
+    document.querySelector('#cart12').classList.remove('close');
+    document.querySelector('#cart1').classList.toggle('close');
+    document.querySelector('#cart2').classList.toggle('close');
+    document.querySelector('#cart3').classList.toggle('close');
+    document.querySelector('#cart4').classList.toggle('close');
+    document.querySelector('#cart5').classList.toggle('close');
+    document.querySelector('#cart6').classList.toggle('close');
+    document.querySelector('#cart7').classList.toggle('close');
+    document.querySelector('#cart8').classList.toggle('close');
+    document.querySelector('#cart9').classList.toggle('close');
+    document.querySelector('#cart11').classList.toggle('close');
+    document.querySelector('#cart12').classList.toggle('close');
+}
+
+document.querySelector('#industry11').onclick = function(){
+    document.querySelector('#cart1').classList.remove('close');
+    document.querySelector('#cart2').classList.remove('close');
+    document.querySelector('#cart3').classList.remove('close');
+    document.querySelector('#cart4').classList.remove('close');
+    document.querySelector('#cart5').classList.remove('close');
+    document.querySelector('#cart6').classList.remove('close');
+    document.querySelector('#cart7').classList.remove('close');
+    document.querySelector('#cart8').classList.remove('close');
+    document.querySelector('#cart9').classList.remove('close');
+    document.querySelector('#cart10').classList.remove('close');
+    document.querySelector('#cart11').classList.remove('close');
+    document.querySelector('#cart12').classList.remove('close');
+    document.querySelector('#cart1').classList.toggle('close');
+    document.querySelector('#cart2').classList.toggle('close');
+    document.querySelector('#cart3').classList.toggle('close');
+    document.querySelector('#cart4').classList.toggle('close');
+    document.querySelector('#cart5').classList.toggle('close');
+    document.querySelector('#cart6').classList.toggle('close');
+    document.querySelector('#cart7').classList.toggle('close');
+    document.querySelector('#cart8').classList.toggle('close');
+    document.querySelector('#cart9').classList.toggle('close');
+    document.querySelector('#cart10').classList.toggle('close');
+    document.querySelector('#cart11').classList.toggle('close');
+    document.querySelector('#cart12').classList.toggle('close');
+}
+
+document.querySelector('#industry12').onclick = function(){
+    document.querySelector('#cart1').classList.remove('close');
+    document.querySelector('#cart2').classList.remove('close');
+    document.querySelector('#cart3').classList.remove('close');
+    document.querySelector('#cart4').classList.remove('close');
+    document.querySelector('#cart5').classList.remove('close');
+    document.querySelector('#cart6').classList.remove('close');
+    document.querySelector('#cart7').classList.remove('close');
+    document.querySelector('#cart8').classList.remove('close');
+    document.querySelector('#cart9').classList.remove('close');
+    document.querySelector('#cart10').classList.remove('close');
+    document.querySelector('#cart11').classList.remove('close');
+    document.querySelector('#cart12').classList.remove('close');
+    document.querySelector('#cart1').classList.toggle('close');
+    document.querySelector('#cart2').classList.toggle('close');
+    document.querySelector('#cart3').classList.toggle('close');
+    document.querySelector('#cart4').classList.toggle('close');
+    document.querySelector('#cart5').classList.toggle('close');
+    document.querySelector('#cart6').classList.toggle('close');
+    document.querySelector('#cart7').classList.toggle('close');
+    document.querySelector('#cart8').classList.toggle('close');
+    document.querySelector('#cart9').classList.toggle('close');
+    document.querySelector('#cart10').classList.toggle('close');
+    document.querySelector('#cart12').classList.toggle('close');
+}
+
+document.querySelector('#industry13').onclick = function(){
+    document.querySelector('#cart1').classList.remove('close');
+    document.querySelector('#cart2').classList.remove('close');
+    document.querySelector('#cart3').classList.remove('close');
+    document.querySelector('#cart4').classList.remove('close');
+    document.querySelector('#cart5').classList.remove('close');
+    document.querySelector('#cart6').classList.remove('close');
+    document.querySelector('#cart7').classList.remove('close');
+    document.querySelector('#cart8').classList.remove('close');
+    document.querySelector('#cart9').classList.remove('close');
+    document.querySelector('#cart10').classList.remove('close');
+    document.querySelector('#cart11').classList.remove('close');
+    document.querySelector('#cart12').classList.remove('close');
+    document.querySelector('#cart2').classList.toggle('close');
+    document.querySelector('#cart3').classList.toggle('close');
+    document.querySelector('#cart4').classList.toggle('close');
+    document.querySelector('#cart5').classList.toggle('close');
+    document.querySelector('#cart6').classList.toggle('close');
+    document.querySelector('#cart7').classList.toggle('close');
+    document.querySelector('#cart8').classList.toggle('close');
+    document.querySelector('#cart9').classList.toggle('close');
+    document.querySelector('#cart10').classList.toggle('close');
+    document.querySelector('#cart11').classList.toggle('close');
+    document.querySelector('#cart12').classList.toggle('close');
+}
+
+   
+
